e2fa1b7396fdc161e7dc9935c08997cb20b85b6f
Brian Crookshanks <zeron30@hotmail.com>
2024-01-05 09:04:58 -0700

    establishing header file creation

--- front-end/tons-of-tacos/app/page.tsx
+++ front-end/tons-of-tacos/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main>
       <h1>Welcome to Tons Of Tacos! Welcome text placeholder</h1>
       <div>image place holder</div>
+      <div>footer placeholder</div>
     </main>
   );
 }
