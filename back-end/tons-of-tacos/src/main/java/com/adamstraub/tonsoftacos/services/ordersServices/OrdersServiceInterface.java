6a2383c8a002ccb7d51cd8202104fec0322d7704
Brian Crookshanks <zeron30@hotmail.com>
2024-01-15 20:11:36 -0700

    cleaned up repo

--- back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/services/ordersServices/OrdersServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.adamstraub.tonsoftacos.services.ordersServices;
-import com.adamstraub.tonsoftacos.dto.businessDto.NewOrder;
-import com.adamstraub.tonsoftacos.dto.customerDto.ordersDto.OrderReturnedToCustomer;
-import org.springframework.web.bind.annotation.RequestBody;
-
-public interface OrdersServiceInterface {
-
-    OrderReturnedToCustomer createOrder(@RequestBody NewOrder order) throws Exception;
-}
