e5ff65574872d9285fd9c7a3329a29262b4bfcd0
Brian Crookshanks <zeron30@hotmail.com>
2024-03-13 15:26:47 -0700

    loading animation reintroduced

--- front-end/tons-of-tacos/components/menu/menu-items/menu-item-list.tsx
+++ front-end/tons-of-tacos/components/menu/menu-items/menu-item-list.tsx
@@ -1,11 +1,11 @@
-import { useMenuItemsForCategory } from "@/lib/menu";
 import MenuItem from "./menu-item";
 import classes from "./menu-item-list.module.css";
-import { useEffect, useRef } from "react";
 
-export default function MenuItemList(menuItems: { menuItems: MenuItem[] }) {
+export default async function MenuItemList(menuItems: {
+  menuItems: MenuItem[];
+}) {
   const items = menuItems.menuItems;
-
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <ul className={classes.grid}>
       {items.map(
