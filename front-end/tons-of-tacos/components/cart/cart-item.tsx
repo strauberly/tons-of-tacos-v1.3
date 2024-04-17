ef1d0d957b7727e348179feaad3468e3f53f6426
Brian Crookshanks <zeron30@hotmail.com>
2024-04-17 06:08:30 -0700

    fleshing out cart item

--- front-end/tons-of-tacos/components/cart/cart-item.tsx
+++ front-end/tons-of-tacos/components/cart/cart-item.tsx
@@ -1,8 +1,40 @@
-export default function CartItem() {
+import { useState } from "react";
+import QuantitySelector from "../menu/menu-items/quantity-selector/quantity-selector";
+
+export default function CartItem(props: {
+  itemQuantity: number;
+  itemName: string;
+  itemPrice: number;
+  size: string;
+}) {
+  const [quantity, setQuantity] = useState(props.itemQuantity);
+  // this will be changed to get quantity from item stowed in cart
+  // const itemQuantity: number = 1;
+
+  const increment = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrement = () => {
+    setQuantity(quantity - 1);
+  };
+
+  const adjPrice: number =
+    props.itemPrice - (props.itemPrice / props.itemQuantity) * quantity;
+
   return (
     <div>
+      <>{props.itemName}</>
       <p>item name placeholder</p>
+      <QuantitySelector
+        value={quantity}
+        increment={increment}
+        decrement={decrement}
+      />
       <>quantity selector placeholder</>
+      <>{props.size}</>
+      <>size if it exists placeholder</>
+      <>{adjPrice}</>
       <>item price place holder</>
     </div>
   );
