a9b4b76dd2f407bf3a0ef91049b5eed3ad514574
Brian Crookshanks <zeron30@hotmail.com>
2024-06-18 20:02:59 -0700

    initialised customer info form files

---
+++ front-end/tons-of-tacos/components/ui/forms/customer-info-form.tsx
@@ -0,0 +1,13 @@
+import Card from "../cards/card";
+
+export default function CustomerInfoForm() {
+  return (
+    <>
+      <p>instructions placeholder</p>
+      <p>first name place holder</p>
+      <p>email placeholder</p>
+      <p>phone placeholder</p>
+      <p> submit placeholder</p>
+    </>
+  );
+}
